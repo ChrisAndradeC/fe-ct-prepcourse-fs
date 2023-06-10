784740516e45e4c76a6559876dd8b3d9d52783f9
Christian Andrade <an153473@gmail.com>
2023-06-09 19:00:21 -0500

    HW Callbacks

--- M05 JavaScript Arrays/homework.js
+++ M05 JavaScript Arrays/homework.js
@@ -3,34 +3,46 @@
 function devolverPrimerElemento(array) {
    // Retornar el primer elemento del arreglo recibido por parámetro.
    // Tu código:
+   return array[0];
 }
 
 function devolverUltimoElemento(array) {
    // Retornar el último elemento del arreglo recibido por parámetro.
    // Tu código:
+   return array[array.length -1];
 }
 
 function obtenerLargoDelArray(array) {
    // Retornar la longitud del arreglo recibido por parámetro.
    // Tu código:
+   return array.length;
 }
 
 function incrementarPorUno(array) {
    // El arreglo recibido por parámetro contiene números.
    // Retornar un arreglo con los elementos incrementados en +1.
    // Tu código:
+   var nuevoArray
+   for( let i = 0; i < array.length; i++){
+      nuevoArray.push(array[i] + 1);
+   }
+   return nuevoArray;
 }
 
 function agregarItemAlFinalDelArray(array, elemento) {
    // Agrega el "elemento" al final del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.push(elemento);
+   return array;
 }
 
 function agregarItemAlComienzoDelArray(array, elemento) {
    // Agrega el "elemento" al comienzo del arreglo recibido.
    // Retorna el arreglo.
    // Tu código:
+   array.unshift(elemento);
+   return array;
 }
 
 function dePalabrasAFrase(palabras) {
@@ -39,42 +51,73 @@ function dePalabrasAFrase(palabras) {
    // con un espacio entre cada palabra.
    // Ejemplo: ['Hello', 'world!'] -> 'Hello world!'.
    // Tu código:
+   return palabras.join(' ');
 }
 
 function arrayContiene(array, elemento) {
    // Verifica si el elemento existe dentro del arreglo recibido.
-   // Retornar True si está, o False si no está.
+   // Retornar true si está, o false si no está.
    // Tu código:
+   return array.includes(elemento);
 }
 
 function agregarNumeros(arrayOfNums) {
    // El parámetro "arrayOfNums" debe ser un arreglo de números.
    // Suma todos los elementos y retorna el resultado.
    // Tu código:
+   return arrayOfNums.reduce(function(acumulador, elemento){
+		return acumulador + elemento;
+},0)
 }
 
 function promedioResultadosTest(resultadosTest) {
    // El parámetro "resultadosTest" es un arreglo de números.
    // Itera (en un bucle) los elementos del arreglo y devuelve el promedio de las notas.
    // Tu código:
+   
+   return agregarNumeros(resultadosTest) / resultadosTest.length
+
 }
 
 function numeroMasGrande(arrayOfNums) {
    // El parámetro "arrayOfNums" es un arreglo de números.
    // Retornar el número más grande.
    // Tu código:
-}
+   var max = 0;
+
+   for (let i = 0; index < arrayOfNums.length; index++) {
+      if(arrayOfNums[i] > max) max = arrayOfNums[i];
+      }
+   return max; 
+
+   }
 
 function multiplicarArgumentos() {
    // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto.
    // Si no se pasan argumentos retorna 0. Si se pasa un argumento, simplemente retórnalo.
    // [PISTA]: "arguments" es un arreglo.
    // Tu código:
+   var producto = 1;
+
+   for (let i = 0; i < arguments.length; i++) {
+     producto = producto * arguments[i];
+      
+   }
+
+   return producto;
+
 }
 
 function cuentoElementos(array) {
    // Desarrolla una función que retorne la cantidad de elementos del arreglo cuyo valor sea mayor que 18.
    // Tu código:
+
+   var count = 0;
+   array.forEach((element) => {
+      if(element > 18) count ++;
+   });
+
+   return count;
 }
 
 function diaDeLaSemana(numeroDeDia) {
@@ -82,18 +125,24 @@ function diaDeLaSemana(numeroDeDia) {
    // Realiza una función que, dado el número del día de la semana, retorne: "Es fin de semana"
    // si el día corresponde a "Sábado" o "Domingo", y "Es dia laboral" en caso contrario.
    // Tu código:
+   if(numeroDeDia === 1 || numeroDeDia === 7) return "Es fin de semana";
+   return "Es dia laboral";
 }
 
 function empiezaConNueve(num) {
    // Esta función recibe por parámetro un número.
-   // Debe retornar True si el entero inicia con 9 y False en otro caso.
+   // Debe retornar true si el entero inicia con 9 y false en otro caso.
    // Tu código:
+   return num.toString()[0] == '9';
 }
 
 function todosIguales(array) {
-   // Si todos los elementos del arreglo son iguales, retornar True.
-   // Caso contrario retornar False.
+   // Si todos los elementos del arreglo son iguales, retornar true.
+   // Caso contrario retornar false.
    // Tu código:
+   array.every((num) => {
+      return num === num;
+   });
 }
 
 function mesesDelAño(array) {
@@ -101,18 +150,37 @@ function mesesDelAño(array) {
    // "Marzo" y "Noviembre", guardarlos en un nuevo arreglo y retornarlo.
    // Si alguno de los meses no está, retornar el string: "No se encontraron los meses pedidos".
    // Tu código:
+   const mesesPedidos = array.map((mes) => {
+      if (mes === "Enero" || mes === "Marzo" || mes === "Noviembre") {
+         return mes;
+      }
+   });
+
+   if (mesesPedidos.includes("Enero") && mesesPedidos.includes("Marzo") && mesesPedidos.includes("Noviembre")) {
+      return mesesPedidos;
+   } else {
+      return "No se encontraron los meses pedidos";
+   }
 }
 
 function tablaDelSeis() {
    // Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
    // La función devuelve un arreglo con los resultados de la tabla de multiplicar del 6 en orden creciente.
    // Tu código:
+
+   var resultado = [];
+   for (let i = 0; i <= 10; i++) {
+      resultado.push(6 * i);
+      
+   }
 }
 
 function mayorACien(array) {
    // La función recibe un arreglo con enteros entre 0 y 200.
    // Recorrerlo y retornar un arreglo con todos los valores mayores a 100 (no incluye el 100).
    // Tu código:
+
+   return array.filter((elemento) => elemento > 100);
 }
 
 /* ----------------------------------------------------------------------------------
---
+++ prueba.js
@@ -0,0 +1,8 @@
+var usuario = {
+	nombre:'Andres',
+	edad:26,
+}
+
+usuario['email'] = 'correo@email.com'
+
+console.log(usuario)
